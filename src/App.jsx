54ff18b975c57b1b8c24ed4b473523963b9cb121
Kalle <kalleniemi247@gmail.com>
2024-04-20 15:16:13 +0300

    Upload react project

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import { Alert, Box, Button, Card, Input, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
+
+function App() {
+
+  const [inputText, setInputText] = useState('')
+  const [textLog, setTextLog] = useState([])
+
+  const fakeSentimentAnalysis = (text) => {
+    // This is just a fake function to simulate the sentiment analysis
+    const input = text;
+    const randomFloat = Math.random();
+    console.log(randomFloat);
+    if (randomFloat > 0.5) {
+      return 'positive';
+    } else if (randomFloat < 0.5) {
+      return 'negative';
+    } else {
+      return 'unknown';
+    }
+  };
+
+  const handleButton = () => {
+
+    // Check if the input text is empty
+    if (inputText === '') {
+      return
+    }
+
+    try {
+      // Analyze and reformat the output
+      const sentiment = fakeSentimentAnalysis(inputText)
+      const analysis = {
+        text: inputText,
+        sentiment: sentiment,
+      }
+
+      // Push the new text to the log
+      const newTextLog = [analysis, ...textLog]
+      setTextLog(newTextLog)
+      console.log(newTextLog)   
+      
+      // Clear the input text
+      setInputText('')
+
+    } catch (error) {
+      
+    }
+
+  }
+
+  return (
+    <>
+    <Box sx={{maxWidth: 500, maxHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
+      <Card sx={{m:2, flexShrink:0, p:2}}>
+        <Typography variant="h5">Sentiment analysis front-end</Typography>
+        <Typography variant="body1">This model will predict the overall verdict of your sentence.</Typography>
+        <Stack sx={{mt:2}} direction='row'>
+          <Input
+           fullWidth
+           placeholder="Type something here"
+           onChange={(event) => setInputText(event.target.value)}
+           onKeyDown={(event) => {if (event.key === 'Enter') handleButton()}}
+           value={inputText}
+          />
+          <Button
+           onClick={handleButton}
+           >
+            Predict
+          </Button>
+        </Stack>
+        <Typography sx={{mt:2}} variant="body2" color='warning.main'>Fake analysis only—no connection to back-end yet.</Typography>
+      </Card>
+      <Box sx={{flexGrow:0, flexShrink: 1, overflowY: 'auto'}}>
+        {textLog.map((analysis, index) => (
+          <SentimentInfo key={index} sentiment={analysis.sentiment}>
+            {analysis.text}
+          </SentimentInfo>
+        ))}
+      </Box>
+    </Box>
+    </>
+  )
+}
+
+function SentimentInfo({children, sentiment}) {
+
+  let severity = 'info'
+  if (sentiment === 'positive') {
+    severity = 'success'
+  } else if (sentiment === 'negative') {
+    severity = 'error'
+  }
+
+  return (
+    <Alert sx={{m:2}} icon={<></>} severity={severity}>
+      <Stack direction='column'>
+        <Typography variant='body1'>
+          {children}
+        </Typography>
+        <Typography variant='button'>
+          {sentiment}
+        </Typography>
+      </Stack>
+    </Alert>
+  )
+}
+
+export default App
